e3c96b613b7488a935e51da5b005673c746460be
Björn Jacob <bjoern.jacob@tritum.de>
2019-12-18 16:36:33 +0100

    [TASK] Add compatibility for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,7 @@
 <?php
-
 $EM_CONF['repeatable_form_elements'] = [
     'title' => 'Repeatable form elements',
-    'description' => '',
+    'description' => 'Adds a new form element which allows the editor to create a new, in the frontend repeatable container for nesting elements.',
     'category' => '',
     'state' => 'stable',
     'uploadfolder' => 0,
@@ -10,10 +9,10 @@ $EM_CONF['repeatable_form_elements'] = [
     'clearCacheOnLoad' => 0,
     'author' => 'Ralf Zimmermann TRITUM GmbH',
     'author_email' => 'ralf.zimmermann@tritum.de',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.2.0',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
