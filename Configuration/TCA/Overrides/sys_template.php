b0b2bbdd765ba36cf0f7d257c871fedac9ee2dee
Elias Häußler <elias@haeussler.dev>
2022-07-26 20:45:50 +0200

    [TASK] Reference extension key constant in TCA

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        'repeatable_form_elements',
+        \TRITUM\RepeatableFormElements\Configuration\Extension::KEY,
         'Configuration/TypoScript',
         'Repeatable form configuration'
     );
