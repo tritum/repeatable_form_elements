a36073233461545064bc045da33c06903ea34c28
Björn Jacob <bjoern.jacob@tritum.de>
2022-08-16 14:30:02 +0200

    [TASK] Cleanup of composer.json and ext_emconf.php
    The above mentioned files have been cleaned up/
    streamlined to be in line with the current standard.
    
    Furthermore, the version has been bumped to 3.1.0.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,13 @@
 <?php
 $EM_CONF['repeatable_form_elements'] = [
     'title' => 'Repeatable form elements',
-    'description' => 'Adds a new form element which allows the editor to create a new, in the frontend repeatable container for nesting elements.',
-    'category' => '',
+    'description' => 'Adds a new form element which allows the editor to create new container elements with any type fields in them. In the frontend, a user can create any number of new containers. This is an extension for TYPO3 CMS.',
+    'category' => 'fe',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'author' => 'Ralf Zimmermann TRITUM GmbH',
-    'author_email' => 'ralf.zimmermann@tritum.de',
-    'version' => '3.0.0',
+    'author' => 'Ralf Zimmermann, Elias Häußler',
+    'author_email' => 'ralf.zimmermann@tritum.de, elias@haeussler.dev',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.13-11.99.99',
