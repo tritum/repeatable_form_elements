46e7dea634f0cebacb9fbfa35bbbb80f29b95f20
Uwe <wiebach@kapelan.com>
2024-05-10 11:09:11 +0200

    Update RepeatableContainer.js
    Wrong hidden selector...

--- Resources/Public/JavaScript/Form/Frontend/RepeatableContainer.js
+++ Resources/Public/JavaScript/Form/Frontend/RepeatableContainer.js
@@ -21,7 +21,7 @@ $(function() {
 
         $containerClone
             .find('input')
-                .filter(':not(:checkbox, :radio, :hidden)').attr('value', '').end()
+                .filter(':not(:checkbox, :radio, [type="hidden"])').attr('value', '').end()
                 .filter(':checkbox, :radio').removeAttr('checked').end().end()
             .find('textarea').attr('value', '').end()
             .find('select').prop("selectedIndex", -1).find('option:selected').removeAttr('selected');
