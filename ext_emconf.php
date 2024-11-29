982a56f454cc3b52947cbdd8243b2e4916b35204
Tobias Jungmann <186044798+dreistromland-tj@users.noreply.github.com>
2024-11-29 08:57:27 +0100

    [!!!][TASK] Update dependencies to be compatible with TYPO3 v13, drop v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,10 +6,10 @@ $EM_CONF['repeatable_form_elements'] = [
     'state' => 'stable',
     'author' => 'Ralf Zimmermann, Elias Häußler, Christian Seyfferth',
     'author_email' => 'r.zimmermann@dreistrom.land, elias@haeussler.dev, c.seyfferth@dreistrom.land',
-    'version' => '4.0.1',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.23-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
